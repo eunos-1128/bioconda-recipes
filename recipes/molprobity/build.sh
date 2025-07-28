a37084a87ce13d96c1c497eaeecd5238d6bb2922
UENO, M. <eunosium-1128@live.jp>
2025-07-28 00:43:11 +0000

    molprobity: Update build script and meta.yaml for MolProbity installation

--- recipes/molprobity/build.sh
+++ recipes/molprobity/build.sh
@@ -2,50 +2,51 @@
 
 set -exo pipefail
 
-export CFLAGS="${CFLAGS} -include stdlib.h"
-export CXXFLAGS="${CXXFLAGS} -O3"
+# export CFLAGS="${CFLAGS} -include stdlib.h"
+# export CXXFLAGS="${CXXFLAGS} -O3"
 
-if [[ "${build_platform}" == "linux-"* ]]; then
-    export CXXFLAGS="${CXXFLAGS} -fopenmp"
-    export LDFLAGS="${LDFLAGS} -fopenmp"
-elif
-    [[ "${build_platform}" == "osx-"* ]]; then
-    export CXXFLAGS="${CXXFLAGS} -Xpreprocessor -fopenmp"
-    export LDFLAGS="${LDFLAGS} -lomp"
-fi
+# if [[ "${build_platform}" == "linux-"* ]]; then
+#     export CXXFLAGS="${CXXFLAGS} -fopenmp"
+#     export LDFLAGS="${LDFLAGS} -fopenmp"
+# elif
+#     [[ "${build_platform}" == "osx-"* ]]; then
+#     export CXXFLAGS="${CXXFLAGS} -Xpreprocessor -fopenmp"
+#     export LDFLAGS="${LDFLAGS} -lomp"
+# fi
 
-sed -i.bak 's|python bootstrap.py --builder=molprobity --use-conda $nproc||' install_via_bootstrap.sh
-sed -i.bak 's|echo ++++++++++ MolProbity configure.sh finished.||' install_via_bootstrap.sh
-sed -i.bak 's|echo ++++++++++ Run molprobity/setup.sh to complete installation.||' install_via_bootstrap.sh
+# sed -i.bak 's|python bootstrap.py --builder=molprobity --use-conda $nproc||' install_via_bootstrap.sh
+# sed -i.bak 's|echo ++++++++++ MolProbity configure.sh finished.||' install_via_bootstrap.sh
+# sed -i.bak 's|echo ++++++++++ Run molprobity/setup.sh to complete installation.||' install_via_bootstrap.sh
 
-chmod +x install_via_bootstrap.sh
-./install_via_bootstrap.sh "${CPU_COUNT}"
+# chmod +x install_via_bootstrap.sh
+# ./install_via_bootstrap.sh "${CPU_COUNT}"
 
-sed -i.bak '/^from __future__ import absolute_import, division, print_function/a\
-import os\
-os.environ.setdefault("CPPFLAGS", "")\
-os.environ["CPPFLAGS"] += " -include stdlib.h"\
-' bootstrap.py
+# sed -i.bak '/^from __future__ import absolute_import, division, print_function/a\
+# import os\
+# os.environ.setdefault("CPPFLAGS", "")\
+# os.environ["CPPFLAGS"] += " -include stdlib.h"\
+# ' bootstrap.py
 
-"${PYTHON}" bootstrap.py \
-    --download-only \
-    --builder=molprobity \
-    --use-conda="${PREFIX}" \
-    --nproc="${CPU_COUNT}"
+# "${PYTHON}" bootstrap.py \
+#     --download-only \
+#     --builder=molprobity \
+#     --use-conda="${PREFIX}" \
+#     --nproc="${CPU_COUNT}"
 
-sed -i.bak 's|int putenv ();||' modules/ccp4io/libccp4/ccp4/library_utils.c
-sed -i.bak -E \
-  's|\(void[[:space:]]*\(\*[[:space:]]*routne\)[[:space:]]*\(\)[[:space:]]*,|(void (* routne)(...),|g' \
-  modules/ccp4io/libccp4/fortran/library_f.c
+# sed -i.bak 's|int putenv ();||' modules/ccp4io/libccp4/ccp4/library_utils.c
+# sed -i.bak -E \
+#   's|\(void[[:space:]]*\(\*[[:space:]]*routne\)[[:space:]]*\(\)[[:space:]]*,|(void (* routne)(...),|g' \
+#   modules/ccp4io/libccp4/fortran/library_f.c
 
-"${PYTHON}" bootstrap.py \
-    --mpi-build \
-    --no-boost-src \
-    --enable-shared \
-    --builder=molprobity \
-    --with-python="${PYTHON}" \
-    --use-conda="${PREFIX}" \
-    --nproc="${CPU_COUNT}" \
-    build tests doc
+# "${PYTHON}" bootstrap.py \
+#     --mpi-build \
+#     --no-boost-src \
+#     --enable-shared \
+#     --builder=molprobity \
+#     --with-python="${PYTHON}" \
+#     --use-conda="${PREFIX}" \
+#     --nproc="${CPU_COUNT}" \
+#     base build tests doc
 
+mkdir -p "${PREFIX}/bin"
 install -m 0755 "${SRC_DIR}/cmdline/"* "${PREFIX}/bin/"
